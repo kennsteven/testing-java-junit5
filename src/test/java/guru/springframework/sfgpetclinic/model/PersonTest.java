b773a50d015d994417f21194406c756df344140f
kenny_alvarado <akennsteven@gmail.com>
2022-05-28 13:48:38 +0200

    first Junit clasess

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,32 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest  {
+
+    @Test
+    void groupedAssertions() {
+        //given
+        Person person = new Person(1l, "Joe", "Buck");
+
+        //then
+        assertAll("Test Prop Set",
+                () -> assertEquals("Joe", person.getFirstName()),
+                () -> assertEquals("Buck", person.getLastName())
+        );
+    }
+
+    @Test
+    void groupedAssertionsMsg() {
+        //given
+        Person person = new Person(1l, "Joe", "Buck");
+
+        //then
+        assertAll("Test Prop Set",
+                () -> assertEquals("Joe", person.getFirstName(), "First name Failed"),
+                () -> assertEquals("Buck", person.getLastName(), "Last name failed")
+                );
+    }
+}
