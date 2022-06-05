26d3ca8be34a74e55c45bc208337c72c16296e63
kenny_alvarado <akennsteven@gmail.com>
2022-06-05 08:52:43 +0200

    Enum parameter in test

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,5 +34,12 @@ class OwnerTest implements ModelTests {
     @ValueSource(strings={"Spring", "Framework", "Kenneht"})
     void testValueScore(String val) {
         System.out.println(val);
+    }
+
+    @DisplayName("Enum Source Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @EnumSource(OwnerType.class)
+    void name(OwnerType ownerType) {
+        System.out.println(ownerType);
     }
 }
