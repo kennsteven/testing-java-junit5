3cf46dc9ce8f5cc914aecf03aab68580b61c116c
kenny_alvarado <akennsteven@gmail.com>
2022-06-05 10:32:16 +0200

    args from method

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -5,10 +5,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -58,10 +57,16 @@ class OwnerTest implements ModelTests {
     }
 
 
-    @DisplayName("CSV Input Test")
+    @DisplayName("Method Provider Test")
     @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
-    @CsvFileSource(resources="/input.csv", numLinesToSkip = 1)
-    void csvFileTest(String stateName, int val1, int val2) {
+    @MethodSource("getargs")
+    void fromMethodTest(String stateName, int val1, int val2) {
         System.out.println(stateName + " = " + val1 + ":" + val2);
+    }
+
+    static Stream<Arguments> getargs(){
+        return Stream.of(
+                Arguments.of("FL", 58,6),
+                Arguments.of("OH", 101, 9));
     }
 }
